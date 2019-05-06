fef7c7aef346846f2e945b2baa231c78895db706
onelitghost <49221487+onelitghost@users.noreply.github.com>
2019-05-06 09:16:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ client.loadCommands();
 client.on('ready', () => {
   console.log(`READY Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   client.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`, "Ready", "event");
-  client.user.setActivity(`discord.gg/TYe3U4w`);
+  client.user.setActivity(`pokécord Helper`);
 });
 
 client.on('error', error => {
