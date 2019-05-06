e923718202a56be005badc6f0be9013d732d27da
onelitghost <49221487+onelitghost@users.noreply.github.com>
2019-05-06 09:46:24 +0000

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
   let embed = new Discord.RichEmbed()
     .setTitle("Information")
     .setColor(0xFF4500)
-    .setDescription("PokéCord Helper is a bot aimed to help Pokecord players. It will tell you what Pokemon it is whenever Pokecord spawns one. As such, you no longer have to rack your brains or even search Google. Sometimes, you may just miss a rare Pokemon like this.")
+    .setDescription("PokéCord Helper is a bot to help Pokecord players. It will tell you what Pokemon it is whenever Pokecord spawns one. As such, you no longer have to rack your brains or even search Google. Sometimes, you may just miss a rare Pokemon like this.")
     .addField("Developer", "<@559642661330485304>")
     .addField("How does it work?", "All the images from Pokecord are hashed into short strings, and stored in our database. When Pokecord spawns a Pokemon, PokéCord Helper will hash it, then compare with the database and retrieve the name of the Pokemon.")
     .addField("Bot is blacklisted?", "Don't worry, creating bots is easy. Join our official server to invite the bot, and always get the latest announcements.")
