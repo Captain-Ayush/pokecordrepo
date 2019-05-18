421a87ab5403c30719835a53310f853177aa78d3
onelitghost <49221487+onelitghost@users.noreply.github.com>
2019-05-18 14:06:54 +0000

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -9,9 +9,7 @@ module.exports.run = async (client, message, args) => {
     .addField("How does it work?", "All the images from Pokecord are hashed into short strings, and stored in our database. When Pokecord spawns a Pokemon, PokéCord Helper will hash it, then compare with the database and retrieve the name of the Pokemon.")
     .addField("Bot is blacklisted?", "Don't worry, creating bots is easy. Join our official server to invite the bot, and always get the latest announcements.")
     .addField("You're miles, or a moderator from Pokecord?", "Impressed with my bot? 100% accuracy. Come drop me a DM.")
-    .addField("Bot invite link", "https://discordapp.com/oauth2/authorize?client_id=574878608342908949&scope=bot&permissions=8")
     .addField("Server invite link", "https://discord.gg/MGqP6Tt");
-  
   message.channel.send(embed);
 };
 
