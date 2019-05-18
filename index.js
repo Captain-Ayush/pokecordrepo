7b323876dc61120937fa11df17ba750ddacb24b8
onelitghost <49221487+onelitghost@users.noreply.github.com>
2019-05-18 14:02:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ client.loadCommands();
 client.on('ready', () => {
   console.log(`READY Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   client.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`, "Ready", "event");
-  client.user.setActivity(Pokécord Helper upgraded', {type: "Streaming"});
+  client.user.setActivity('Pokécord Helper upgraded', {type: "Streaming"});
 });
 
 client.on('error', error => {
